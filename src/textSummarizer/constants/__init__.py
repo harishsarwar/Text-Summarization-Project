b9bf6c8c1d24293aba9078b7ab7b300b5efe543d
harish sarwar <mdharishsarwar78@gmail.com>
2024-12-18 15:13:31 +0530

    data ingestion notebook added

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,6 @@
+# This file is used to manage and access the data file dir without any hard code.
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config\config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,11 @@
+# Comming from the "research\01_data_ingestion.ipynb" dir.
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
